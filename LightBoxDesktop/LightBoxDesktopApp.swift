4168cb4b352339d64dd2a636847e37ffbb7a3ad9
Peter Grillet <pagrillet@gmail.com>
2025-02-14 12:07:20 +0000

    Initial Commit

---
+++ LightBoxDesktop/LightBoxDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightBoxDesktopApp.swift
+//  LightBoxDesktop
+//
+//  Created by Peter Grillet on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LightBoxDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LightBoxDesktopTests/LightBoxDesktopTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightBoxDesktopTests.swift
+//  LightBoxDesktopTests
+//
+//  Created by Peter Grillet on 14/02/2025.
+//
+
+import Testing
+@testable import LightBoxDesktop
+
+struct LightBoxDesktopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
